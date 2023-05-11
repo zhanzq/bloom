7fb64b6f29aaeca98148f618ef6665473d690e1a
zhanzq <zhanzhiqiang09@126.com>
2023-05-11 14:42:51 +0800

    1. 添加finetune功能代码 2. 修改generate代码

--- finetune/main.py
+++ finetune/main.py
@@ -9,31 +9,51 @@ import torch
 import torch.nn as nn
 from transformers import AutoTokenizer, BloomForCausalLM
 
-class BLOOM(nn.Module):
+
+class BLOOM:
     def __init__(self,
                  model_name_or_path,
+                 train_epochs=3,
                  max_seq_len=512,
                  ):
         self.tokenizer = AutoTokenizer.from_pretrained(model_name_or_path)
         self.model = BloomForCausalLM.from_pretrained(model_name_or_path)
+        print("model loading finished!")
         self.max_seq_len = max_seq_len
+        self.train_epochs = train_epochs
 
     def encoding(self, prompt):
         inputs = self.tokenizer(prompt, return_tensors="pt")
         return inputs
 
+    def finetune(self, dataset):
+        self.model.train()
+        optimizer = torch.optim.Adam(self.model.parameters(), lr=2e-5)
+        for _ in range(self.train_epochs):
+            for batch in dataset:
+                outputs = self.model(batch)
+                loss = self.calc_loss(None, None)
+                loss.backward()
+                optimizer.step()
+
+    def calc_loss(self, pred, label):
+        # todo implement
+        return None
+
     def generate(self, prompt):
+        self.model.eval()
         inputs = self.encoding(prompt)
-        batch_size, prompt_len = inputs.shape
+        inputs.pop("attention_mask")
+        batch_size, prompt_len = inputs["input_ids"].shape
         max_step = max(1, self.max_seq_len - prompt_len)
         for _ in range(max_step):
             outputs = self.model(**inputs)
             next_token_id = torch.argmax(outputs.logits, -1)[:, -1:]
-            if next_token_id[0,0] == self.tokenizer.eos_token:
+            if next_token_id[0, 0] == self.tokenizer.eos_token:
                 break
             inputs["input_ids"] = torch.cat((inputs["input_ids"], next_token_id), dim=-1)
 
-        tokens = self.tokenizer.convert_ids_to_tokens(next_token_id)
+        tokens = self.tokenizer.convert_ids_to_tokens(inputs["input_ids"][0])
         sequence = self.tokenizer.convert_tokens_to_string(tokens)
 
         return sequence
@@ -41,27 +61,11 @@ class BLOOM(nn.Module):
 
 def main():
     # "bigscience/bloom-560m"
-    model_path_or_flag = "/Users/zhanzq/Downloads/bloom-560m"
-    tokenizer = AutoTokenizer.from_pretrained(model_path_or_flag)
-    model = BloomForCausalLM.from_pretrained(model_path_or_flag)
-
-    inputs = tokenizer("Hello, my dog is cute.", return_tensors="pt")
+    model_name_or_path = "d:\\pretrained_models\\bloom-560m"
     query = "Hello, my dog is cute."
-
-    for _ in range(10):
-        inputs = tokenizer(query, return_tensors="pt")
-
-        # inputs.input_ids = torch.cat((inputs.input_ids, next_token_id), -1)
-        # inputs.attention_mask = torch.cat((inputs.attention_mask, one_mask), -1)
-
-        outputs = model(**inputs)
-        next_token_id = torch.argmax(outputs.logits, -1)[:, -1:]
-        torch.argmax(outputs.logits)
-        next_token = tokenizer.convert_ids_to_tokens(next_token_id)
-        next_word = tokenizer.convert_tokens_to_string(next_token)
-
-        query += next_word
-        print(query)
+    bloom = BLOOM(model_name_or_path, 100)
+    sequence = bloom.generate(prompt=query)
+    print(sequence)
 
     return
 
